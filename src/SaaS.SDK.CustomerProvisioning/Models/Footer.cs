65ee75479602e841057fb2dd4f256c10a2e61813
Prasad Yalla <prasad.yalla@spektrasystems.com>
2020-04-21 14:59:54 +0530

    Logo and footer text is getting from database

---
+++ src/SaaS.SDK.CustomerProvisioning/Models/Footer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Marketplace.SaaS.SDK.CustomerProvisioning.Models
+{
+    public class Footer
+    {
+        public string Footertext { get; set; }
+    }
+}
---
+++ src/SaaS.SDK.PublisherSolution/Controllers/LogoViewComponent.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Marketplace.Saas.Web.Models;
+using Microsoft.Marketplace.SaasKit.Client.DataAccess.Context;
+using Microsoft.Marketplace.SaasKit.Client.DataAccess.Contracts;
+using Microsoft.Marketplace.SaasKit.Client.DataAccess.Services;
+using Microsoft.Marketplace.SaasKit.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Microsoft.Marketplace.SaasKit.Client.Controllers
+{
+    public class LogoViewComponent : ViewComponent
+    {
+        /// <summary>
+        /// The Configuration
+        /// </summary>
+        public IConfiguration _iconfiguration { get; }
+        private readonly IApplicationConfigRepository applicationConfigRepository;
+
+        public LogoViewComponent(IConfiguration iconfiguration, IApplicationConfigRepository applicationConfigRepository)
+        {
+            _iconfiguration = iconfiguration;
+            this.applicationConfigRepository = applicationConfigRepository;
+        }
+        /// <summary>
+        /// Load footer using the ViewComponent
+        /// </summary>
+        /// <returns></returns>
+        /// 
+        public IViewComponentResult Invoke()
+        {
+            Logo model = new Logo();
+            model.Logolink = applicationConfigRepository.GetValuefromApplicationConfig("ApplicationLogo");
+            return View("_Logo", model);
+        }
+
+    }
+}
