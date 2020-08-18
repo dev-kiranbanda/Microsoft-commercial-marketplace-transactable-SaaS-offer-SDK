1990ea5b9898037e2b93af3d1014336513c54b6f
Prasad Yalla <prasad.yalla@spektrasystems.com>
2020-08-18 16:52:00 +0530

    Alter script added

---
+++ deployment/Database/Alterscript-3.sql
@@ -0,0 +1,14 @@
+UPDATE ApplicationConfiguration 
+SET value = '<div class="text-center" style="margin-top:100px">           <h1 style="font-size:70px;">BETTER TOGETHER:</h1><br>           <h2>VANTAGE + AZURE</h2>        </div> <hr style="width:500px;background-color:#F37440">'
+WHERE Name ='ClientHomePageContent'
+
+UPDATE ApplicationConfiguration 
+SET value = '<div > <div class = "center"> <a href="https://www.teradata.com/Privacy" target="_blank">Privacy Policy</a> ©2020 Teradata. All Rights Reserved </div> </div>  '
+WHERE name ='Footer'
+
+IF NOT EXISTS (SELECT * FROM DatabaseVersionHistory WHERE VersionNumber = 3)
+BEGIN
+	INSERT INTO DatabaseVersionHistory (VersionNumber,ChangeLog,CreateDate,CreateBy)
+	SELECT 3,'User Home Page Content and Footer script update',GETDATE,''
+END
+GO
